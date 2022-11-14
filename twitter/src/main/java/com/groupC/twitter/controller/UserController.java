a32fecddb62fd9e36a37cf0fdcb0c34d1f687006
ShubhamFareye1 <shubham.patidar1@getfareye.com>
2022-11-14 12:46:34 +0530

    changes

--- twitter/src/main/java/com/groupC/twitter/controller/UserController.java
+++ twitter/src/main/java/com/groupC/twitter/controller/UserController.java
@@ -2,15 +2,11 @@ package com.groupC.twitter.controller;
 
 import com.groupC.twitter.dto.BookmarkDto;
 import com.groupC.twitter.dto.UserDto;
-//import com.groupC.twitter.model.Messages;
-import com.groupC.twitter.model.User;
-import com.groupC.twitter.repository.UserRepository;
 import com.groupC.twitter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
