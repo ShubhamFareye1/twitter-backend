18ca09a373dce8f6c1294efcda8c4204e9d4625a
sajal-fareye <sajal.agrawal1@getfareye.com>
2022-11-15 13:34:40 +0530

    Final Dys

--- twitter/src/main/java/com/groupC/twitter/repository/NotificationRepository.java
+++ twitter/src/main/java/com/groupC/twitter/repository/NotificationRepository.java
@@ -2,11 +2,14 @@ package com.groupC.twitter.repository;
 
 import com.groupC.twitter.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
 
-    public List<String> getMsgByUserId(long userId);
+    @Query("select msg from Notification where user_id = :key")
+    public List<String> findMsgByUserId(@Param("key") Long userId);
 
 }
